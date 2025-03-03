aa82fc3c508ff3254430580a2384531082b9fa41
FantinLNA <fantin.ellena--mehl@epfedu.fr>
2025-03-03 14:59:23 +0100

    initialisation

---
+++ src/main/java/com/epf/persistance/DataSource.java
@@ -0,0 +1,13 @@
+package com.epf.persistance;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataSource {
+    private String id;
+    private String pw;
+
+@Bean
+    
+}
